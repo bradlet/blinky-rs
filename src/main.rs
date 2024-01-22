eec1e0fddf709b2c61105c973c7a9e0577c96615
Bradley Thompson <bradlet2@pdx.edu>
2024-01-21 21:28:49 -0800

    Add blinky logic

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,33 @@ use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 use cortex_m_rt::entry;
-use microbit::board::Board;
+use microbit::{
+	board::Board,
+	hal::{
+		prelude::*,
+		Timer,
+	},
+};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    let _board = Board::take().unwrap();
-    let mut counter = 0u64;
+
+    let mut _board = Board::take().unwrap();
+	let mut timer = Timer::new(_board.TIMER0);
+	
+	let mut isOff = false;
+
+    _board.display_pins.col1.set_high().unwrap();
+
     loop {
-        rprintln!("{}", counter);
-        counter += 1;
+		timer.delay_ms(300u16);
+		isOff = if isOff {
+			_board.display_pins.col1.set_high().unwrap();
+			true
+		} else {
+			_board.display_pins.col1.set_low().unwrap();
+			true
+		}
     }
 }
